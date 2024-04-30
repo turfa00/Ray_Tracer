6506b50c0aba5605149d4bff41ee95a50d329cfc
turfa00 <fabrice.turikumwe@etudiant.univ-perp.fr>
2024-04-30 22:40:40 +0200

    Sphere + Normals

--- main.cpp
+++ main.cpp
@@ -4,8 +4,33 @@
 
 #include <iostream>
 
+double hit_sphere(const point3 &center, double radius, const ray &r)
+{
+    vec3 oc = center - r.origin();
+    auto a = r.direction().length_squared();
+    auto h = dot(r.direction(), oc);
+    auto c = oc.length_squared() - radius * radius;
+    auto discriminant = h * h - a * c;
+
+    if (discriminant < 0)
+    {
+        return -1.0;
+    }
+    else
+    {
+        return (h - sqrt(discriminant)) / a;
+    }
+}
+
 color ray_color(const ray &r)
 {
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0)
+    {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
